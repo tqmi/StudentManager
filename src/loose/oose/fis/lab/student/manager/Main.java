1fbdd713d498687178099f7fdc5274fbf9fe1654
Drago <54283756+dragoseflc@users.noreply.github.com>
2020-03-26 15:29:49 +0200

    “Changed the application title to contain the company name
    Co-Authored-By: tqmi <45799508+tqmi@users.noreply.github.com>

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root =
                 FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-        primaryStage.setTitle("Student Manager");
+        primaryStage.setTitle("FIS - Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
